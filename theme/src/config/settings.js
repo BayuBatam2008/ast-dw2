bf5a6b816b4bc7cc7a63f188774b8af345533b17
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2022-12-10 19:27:32 +0700

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -3,6 +3,6 @@ export default {
 	description: `Kelas dengan siswa yang sangat random, terlalu pintar, dan jago coding.`,
 	url: `https://9a.palpale.xyz`, // No trailing slash!
 	name: `9A (XV)`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
