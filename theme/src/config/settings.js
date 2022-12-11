642e0885889514dbc8d65de8f16c7376f4a4e208
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2022-12-11 17:17:30 +0700

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -4,5 +4,5 @@ export default {
 	url: `https://9a.palpale.xyz`, // No trailing slash!
 	name: `9A (XV)`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
-  showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
