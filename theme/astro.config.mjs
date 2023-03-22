cea8d1ee914448f7b312e03fdbfac21413164927
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2023-03-22 18:42:34 +0700

    Update astro.config.mjs

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import lit from "@astrojs/lit";
@@ -8,7 +7,6 @@ import lit from "@astrojs/lit";
 export default defineConfig({
   site: 'https://dw2.papale.my.id',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: false,
   // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
