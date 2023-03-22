4ddbf5ab47a5136878accdbdaa0cb54c68fa7b4b
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2023-03-22 17:42:09 +0700

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Kelas 9A (XV)`,
-	description: `Kelas dengan siswa yang sangat random, terlalu pintar, dan jago coding.`,
-	url: `https://9a.palpale.xyz`, // No trailing slash!
-	name: `9A (XV)`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Arsip DW2`,
+	description: `Website berisi arsip sejarah DW2 dan taktik yang dipakai oleh para intelijen.`,
+	url: `https://dw2.papale.my.id`, // No trailing slash!
+	name: `DW2`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
