6687f6e29817ef766ce1436835916c58a61d8c29
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2023-03-23 09:36:01 +0700

    Update footer.js

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,65 +1,27 @@
 export const footerSocials = [
   {
     name: 'Github',
-    url: 'https://github.com/BayuBatam2008/9a-v2',
+    url: 'https://github.com/BayuBatam2008/ast-dw2',
     icon: "mdi:github",
   },
 ]
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
+		title: 'SPMP',
 		items: [
 			{
-				title: 'Landing Page 1',
+				title: 'General',
 				slug: '/landing-pages/landing-1',
 			},
 			{
-				title: 'Landing Page 2',
+				title: 'Meeting',
 				slug: '/landing-pages/landing-2',
 			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
-		title: 'Company',
-		items: [
-			{
-				title: 'About',
-				slug: '/company/about',
-			},
-			{
-				title: 'Blog',
-				slug: '/blog',
-			},
-			{
-				title: 'Contact',
-				slug: '/company/contact',
-			},
-		],
-	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
+                        {
+			 	title: 'Laporan',
+			 	slug: '/landing-pages/landing-3',
+		        },
 		],
 	},
 ];
