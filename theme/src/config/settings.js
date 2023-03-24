9791cc4c1db258df802c72d7de1d1af713e85cf5
BayuBatam2008 <61579584+BayuBatam2008@users.noreply.github.com>
2023-03-24 11:39:54 +0700

    Update settings.js

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Arsip DW2`,
-	description: `Website berisi arsip sejarah DW2 dan taktik yang dipakai oleh para intelijen.`,
+	description: `Website berisi arsip sejarah DW2. Website ini berisi Konversasi antara 2 pihak beserta taktik yang mereka gunakan untuk melawan pihak lain.`,
 	url: `https://dw2.papale.my.id`, // No trailing slash!
 	name: `DW2`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
